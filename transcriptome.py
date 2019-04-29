620062afd93e2bafef523b2689be494d51ffa042
Drbiology <42206757+Drbiology@users.noreply.github.com>
2019-04-29 10:26:46 +0800

    Update transcriptome.py

--- transcriptome.py
+++ transcriptome.py
@@ -9,9 +9,9 @@ def main():
 	print ''
 	print "##############################################################################################"
 	print ''
-	print "   hisat2 + sam2bam + bam2sort + HTseq + RNA-edit"
+	print "   hisat2 + sam2bam + bam2sort + Stringtie + RNA-edit + bedtools"
 	print ''
-	print "   need software (hisat2 samtools htseq-count and SPRINT) ; "
+	print "   need hisat2 samtools Stringtie and SPRINT ; "
 	print "   need to change 'sp'(sprint_from_bam of SPRINT) and 'samtools_path(samtools path of SPRINT)'"
 	print ''
 	print "   https://github.com/Drbiology"
@@ -25,21 +25,21 @@ def main():
         	print ""
         	print "   Usage:"
         	print ""
-        	print "      python <script.py> id(fastq) reference_genome(.fa) fastq_path gtf_path outfile_path "
+        	print "      python	transcriptome.py  id(fastq_id)	reference_genome(.fa)  fastq_file_path   gtf_file_path  outfile_path "
         	print ""
         	print "   Example: "
         	print ""
         	print "      python hisat.py ID \ "
-		print "			/home/ref_genome/Setaria_italica.Setaria_italica_v2.0.dna.toplevel.fa \ "
-		print "			/home/guzi/fastq_data/ \ "
-		print "			/home/gtf/Setaria_italica.Setaria_italica_v2.0.42.gtf \ "
+		print "			/home/ref_genome/genome.fa \ "
+		print "			/home/fastq_data/ \ "
+		print "			/home/gtf/~.gtf \ "
 		print "			/home/result "
         	print ""
         	print ""
         	#print sys.argv[0]
         
 		sys.exit(0)
-	if len(sys.argv)<5:
+	if len(sys.argv)<2:
         	#print sys.argv[0]
         	help_doc()
 	
@@ -49,22 +49,22 @@ def main():
 	#args[3] : fastq file path
 	#args[4] : gtf file path
 	#args[5] : outfile path
-	##############################
-	### hisat2 ###################
+	#############################
+	### hisat2
 	index=args[2]
 	fqdir=args[3]
-	### Rna-edit #################
+	### Rna-edit
 	rna_edit_path='Rna-edit'
-	sp='./software/SPRINT/bin/sprint_from_bam'
-	samtools_path='./software/SPRINT/samtools_and_bwa/samtools'	### samtools of SPRINT
-	### HTseq ####################
+	sp='/home/software/SPRINT/bin/sprint_from_bam'
+	samtools_path='/home/software/SPRINT/samtools_and_bwa/samtools'	### samtools of SPRINT
+	### stringtie
 	gtf=args[4]
 	
 	def hisat2(index,fqdir,sample_id):
 		return os.system('hisat2 -x {0} \
 		-p 8 \
-		-1 {1}/{2}_r1.fq \
-		-2 {1}/{2}_r2.fq \
+		-1 {1}/{2}_1.P.fq \
+		-2 {1}/{2}_1.P.fq \
 		-S {2}.sam \
 		1>{2}.log \
 		2>{2}.align.summary.txt'.format(index,fqdir,sample_id))
@@ -82,26 +82,29 @@ def main():
 		return os.system('{0} {1}.bam {2} {3} {4}' \
 				.format(sp,sample_id,index,rna_edit_path,samtools_path))
 
-	def htseq(sample_id,gtf):
-		return os.system('htseq-count -f bam {0}.sort.bam \
-				{1} > {0}_count_number.txt'.format(sample_id,gtf))
-	
+	def stringtie(sample_id,gtf):
+		return os.system('stringtie -e -B -p 16 -G {0} -o {1}.gtf {1}.sort.bam'.format(gtf,sample_id))
+
 	def bedtools(gtf,line):
-		return os.system('bedtools -intersect \
+		return os.system('bedtools intersect \
 				-a SPRINT_identified_regular.res \
 				-b {0} \
 				-wb >{1}_edit_annotation'.format(gtf,line))
-	
-	#f=open(args[1],'r')
+
+	f=open(args[1],'r')
 	old_path=os.getcwd()
 	path=args[5]+'/'
 	
-	for line in open(args[1],'r'):
-		line=line.rstrip('\n');sample_id = line
-		os.chdir(path);os.makedirs(path+line);os.chdir(path+line)	### Create working directory and enter it
-		hisat2(index,fqdir,sample_id);samtools(sample_id);htseq(sample_id,gtf)
+	for line in f:
+		line=line.replace('\n','');sample_id = line
+		os.chdir(path)
+		os.makedirs(path+line);
+		os.chdir(path+line)	### Create working directory and enter it
+		hisat2(index,fqdir,sample_id);samtools(sample_id)
+		stringtie(sample_id,gtf)
+		htseq(sample_id,gtf)
 		sprint(sp,sample_id,index,rna_edit_path,samtools_path)
-		os.chdir(rna_edit_path);bedtools(gtf,line)
+		os.chdir(rna_edit_path);#bedtools(gtf,line)
 		os.chdir(old_path)
 
      
@@ -112,9 +115,5 @@ def main():
 main()
 
 
-if __init__ == 'main':
-	main()
-
-
-
-
+if __name__ == 'main':
+	print("Life is short, python is shore")
