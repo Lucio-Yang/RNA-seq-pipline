3e45e99e7af845873ef69db81d6f5cac8c7de38f
Drbiology <42206757+Drbiology@users.noreply.github.com>
2019-04-29 10:27:56 +0800

    Update sra.py

--- sra.py
+++ sra.py
@@ -5,7 +5,7 @@ import shutil
 args=sys.argv
 
 fastq_path=args[1]
-tri_path='./Trimmomatic-0.38/trimmomatic-0.38.jar'
+tri_path='/home/software/Trimmomatic-0.36/trimmomatic-0.36.jar'
 old_path=os.getcwd()
 
 class sra():
@@ -17,14 +17,18 @@ class sra():
 	def sra2fastq(self):
 		return os.system('fastq-dump --split-files {0}.sra'.format(self.id))
 	
-	def trimmomatic(self):
+	def trimmomatic1(self):
+		self.tri_path=tri_path
+		return os.system("java -jar {1} SE -threads 4 \
+			{0}_1.fastq {0}_.P.fq \
+			HEADCROP:18 MINLEN:50 TOPHRED33".format(self.id,self.tri_path))
+	def trimmomatic2(self):
 		self.tri_path=tri_path
 		return os.system("java -jar {1} PE -threads 4 \
 			{0}_1.fastq {0}_2.fastq \
 			{0}_1.P.fq {0}_1.UP.fq \
 			{0}_2.P.fq {0}_2.UP.fq \
 			HEADCROP:18 MINLEN:50 TOPHRED33".format(self.id,self.tri_path))
-	
 	def move(self):
 		try :
 			shutil.move('{0}.sra'.format(self.id), 'sra')
@@ -37,16 +41,19 @@ class sra():
 		except IOError:
 			pass
 def id():
-	return os.system("ls | sed 's/.sra//p' | uniq | sed 's/id//p' > id")
+	return os.system("ls | sed 's/.sra//p' | uniq | sed 's/id//p' | awk NF > id")
 
 def mkdir():
 	return os.system('mkdir -p fastq sra trimmomatic data result')
 		
-os.chdir(fastq_path);id();mkdir()
+os.chdir(fastq_path)
+#id()
+#mkdir()
 for i in open('id','r'):	
 	i = i.rstrip('\n')
 	sra(i,tri_path).sra2fastq()
-	sra(i,tri_path).trimmomatic()
+	sra(i,tri_path).trimmomatic1()
+	#sra(i,tri_path).trimmomatic2()
 	sra(i,tri_path).move()
 os.chdir(old_path)
 
